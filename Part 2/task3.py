9819866fe0a431cc9a7ca3fc2bad2908453467ee
shadowless16 <akdavid4real@gmail.com>
2024-09-27 21:39:18 +0100

    Task2-->Part3

---
+++ Part 2/task3.py
@@ -0,0 +1,19 @@
+import random
+
+def number_guessing_game():
+    target_number = random.randint(1, 10)
+    guess = None
+    
+    print("U Welcome to the Number Guessing Game!")
+    
+    while guess != target_number:
+        guess = int(input("Enter your guess: "))
+
+        if guess < target_number:
+            print("Too low! Try again.")
+        elif guess > target_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number {target_number} correctly.")
+
+number_guessing_game()
