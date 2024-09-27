2fa217be952bfd113ea2b159bcf11c84080f40c2
shadowless16 <akdavid4real@gmail.com>
2024-09-27 20:33:07 +0100

    Task1-->Part1

---
+++ Part 1/task1.py
@@ -0,0 +1,10 @@
+# Task 1: Adding Even Numbers (10 points)
+#  Objective: Write a Python program that calculates the sum of all even numbers between 1 and
+# 100 (inclusive).  Instructions: Use a for loop and the range() function to iterate through the even numbers. Task 2: FizzBuzz Challenge (10 points)
+
+even_numbers = 0
+
+for i in range(2, 101, 2):
+    even_numbers += 1
+
+print(f"The sum of all even numbers between 1 and 100 is: {even_numbers}")
